1a8b372e2d996157a7eaf99a9a806e776717a20c
Sebastian Naranjo <sebas2013n@gmail.com>
2023-05-08 01:20:31 -0500

    Ajustes

--- src/middlewares/authjwt.js
+++ src/middlewares/authjwt.js
@@ -6,7 +6,7 @@ import Role from '../models/Role'
 export const verifyToken = async (req,res,next) => {
     const token = req.headers["x-acces-token"];
 
-    if(!token) return  res.status(403).json('Acceso denegado, el token no existe o expiro.')
+    if(!token) return  res.status(401).json('Acceso denegado, el token no existe o expiro.')
 
     const decoded = jwt.verify(token, config.SECRET)
     console.log(decoded)
